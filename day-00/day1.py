eba5e5f41af11ccc6675ce36ddbb0a416adbfc20
Yokesh <yokeshbaskaran@gmail.com>
2026-05-05 22:18:58 +0530

    input example

--- day-00/day1.py
+++ day-00/day1.py
@@ -25,8 +25,46 @@ a = "10"
 b = "12"
 print(a + b)
 
-
 a = int("10")
 b = int("10")
-print(a + b)
-20
+print(a + b)  # 20
+
+# 5. input from user
+# sample1
+a = input()
+b = input()
+c = a + b
+print("value of c:", c)  # 10 + 20 = 1020 (saves as string)
+
+# sample2
+a = int(input("value of a:"))
+b = int(input("value of b:"))
+c = a + b
+print("value of c:", c)  # 10 + 20 = 30
+
+# sample3
+name = input("enter name:")
+age = input("enter age:")
+address = input("enter address:")
+print("My name is:", name)
+print("My address is:", address)
+
+# sample4
+a = int(input("enter value1:"))
+b = int(input("enter value2:"))
+c = int(input("enter value3:"))
+mutliply = a * b * c
+add = a + b + c
+division = mutliply / add
+print("output:", division)
+
+# sample5
+name = input()
+score = int(input())
+dpt = input()
+print("My name is:", name)
+divided = score / 10
+print("My score is:", divided, "/10")
+# divided = str(score / 10)
+# print("My score is:", divided + "/10")
+print("My dpt is:", dpt)
