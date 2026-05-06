d6eeaf6c95dd322b3165e1a158be8a9c7044a4b4
Yokesh <yokeshbaskaran@gmail.com>
2026-05-06 09:02:01 +0530

    different types in python

--- day-00/day1.py
+++ day-00/day1.py
@@ -4,6 +4,18 @@ print("Python is here")
 print(2 + 4)
 6
 
+# what is variables in python?
+# - In Python, a variable is a name used to store a value in memory, so you can use it later in your program.
+
+# - Think of a variable like a labeled box 📦:
+# - The label is the variable name
+# - The content inside the box is the value
+
+# Rules for naming variables
+# - Must start with a letter or _
+# - Cannot start with a number
+# - Cannot use reserved words (like if, for, while)
+
 # 2. variables
 a = 6
 b = 4
@@ -11,8 +23,23 @@ print(a + b)
 10
 
 # 3. type() -> gives type of variables
-print(type(a))
-# <class 'int'>
+# ---
+# 1. int - 2,5,85
+# 3. float - 25.5,78.6
+# 2. str - "text","rocky"
+# 4. list - nums = [1,2,3]
+# 5. tuple - coords = (10, 20)
+# 6. set - s = {1,2,3}
+# 7. dict - person = {"name": "Alice", "age": 25}
+# 8. bool → True or False
+
+# Quick Summary
+# Numbers → int, float, complex
+# Text → str
+# Collections → list, tuple, set, dict
+# Logical → bool
+# Binary → bytes, etc.
+# print(type(a))
 
 a = "10"
 print(a)
