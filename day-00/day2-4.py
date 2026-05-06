ff45819fbe5f680c16952bdb947988b129f10ad9
Yokesh <yokeshbaskaran@gmail.com>
2026-05-06 15:38:17 +0530

    Python Functions - def

---
+++ day-00/day2-4.py
@@ -0,0 +1,25 @@
+# Python Functions:
+
+# 🔹 What is a Function?
+# - A function is a block of code that runs when you call it.
+# - Instead of writing the same code again and again, you put it inside a function and reuse it.
+
+
+# - Python functions are one of the most important building blocks in programming. They help you organize code, reuse logic, and make programs easier to understand. Let’s walk through the basics in a simple way.
+
+# # Examples
+# def greet():
+#     print("Hello World")
+
+
+# # calling a function called greet
+# greet()
+
+
+def add():
+    a = int(input("Enter a:"))
+    b = int(input("Enter b:"))
+    print("add of a,b:", a + b)
+
+
+add()
