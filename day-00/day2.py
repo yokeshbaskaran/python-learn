e7aa559b99dcb7f98c9a9bb33aab573cac7c30bb
Yokesh <yokeshbaskaran@gmail.com>
2026-05-06 10:55:58 +0530

    comparison - boolean

--- day-00/day2.py
+++ day-00/day2.py
@@ -4,3 +4,23 @@ if age > 18:
     print("You are egilible to vote!")
 else:
     print("You are not egilible to vote!!!")
+
+# Comparison Operators
+# 1. boolean value - True / False = python,js are case sensitive
+data = True
+if data:
+    print("it is true")
+else:
+    print("it is false")
+
+data = False
+if data == False:
+    print("it is false")
+else:
+    print("it is true")
+
+name = input("enter friend name:")
+if name == "Priya":
+    print("Surya meets priya")
+else:
+    print("Surya meets meghna")
