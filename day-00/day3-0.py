a6e5c995f16257b71b8fbbb1cbaefeb0f2fcaa89
Yokesh <yokeshbaskaran@gmail.com>
2026-05-07 12:57:14 +0530

    example in class using q/a

--- day-00/day3-0.py
+++ day-00/day3-0.py
@@ -113,3 +113,128 @@ dell.processor = "11"
 dell.ram = "16GB"
 dell.price = "60000"
 dell.display()
+
+
+# 1)Create a class called student. create a variable = name and register number using constructor. Create a function called display which should display the name and register number of the student
+class Student:
+    def __init__(self):
+        self.name = "nthg"
+        self.regno = "000"
+
+    def printDetail(self):
+        print("name:", self.name)
+        print("reg number:", self.regno)
+
+
+person1 = Student()
+person1.name = "Rocky"
+person1.regno = "CS01"
+person1.printDetail()
+
+person2 = Student()
+person2.name = "singh"
+person2.regno = "CS02"
+person2.printDetail()
+
+# 2)Create a class called Fruit. Create a variable called color using _init method. Create a object called apple "Pass the color variable as a parameter through object".
+
+
+# Sample1
+class fruit:
+    def __init__(self):
+        pass  # Nothing happens during object creation
+
+    def printDetails(self):
+        print("hi")
+
+
+apple = fruit()
+apple.printDetails()
+
+
+# Example
+class Fruit:
+    # self -> refers to current Object ("apple") and col -> refers to variable from apple "yellow"
+    def __init__(self, col):
+        self.color = col
+
+
+apple = Fruit("Yellow")
+print(apple.color)
+
+# 3)Create a class called teacher. Create a variable = name and register number using constructor. Create a function called display which should display the name and register number of the teacher. Create t1 and t2 object and pass the name and reg no value through object.
+
+
+class Teacher:
+    def __init__(self, name, reg):
+        self.name = name
+        self.reg = reg
+
+    def display(self):
+        print("name:", self.name)
+        print("regs:", self.reg)
+
+
+t1 = Teacher("krish", "12")
+t1.display()
+t2 = Teacher("manu", "10")
+t2.display()
+
+# 4)Create a class called calculator. Create 2 variables a and b. Create a function called add,sub,mul,div all functions should take 2 variables as parameter. I Pass the a and b value through object().
+
+
+# Method1 = Best: Cleaner and reusable
+class CalCulate:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def add(self):
+        result = self.a + self.b
+        return result
+
+    def sub(self):
+        result = self.a - self.b
+        return result
+
+    def mul(self):
+        result = self.a * self.b
+        return result
+
+    def div(self):
+        result = self.a / self.b
+        return result
+
+
+n1 = int(input("enter a:"))
+n2 = int(input("enter b:"))
+result = CalCulate(n1, n2)
+print(result.add())
+print(result.sub())
+print(result.mul())
+print(result.div())
+
+
+# Method2 = Medium:Easy to understand and Must pass values every time
+class CalCulate:
+    def add(self, a, b):
+        result = a + b
+        return result
+
+
+result = CalCulate()
+print(result.add(1, 2))
+
+
+# Method3 - ok:without constructor
+class CalCulate:
+    def add(self):
+        return self.a + self.b
+
+
+obj = CalCulate()
+
+obj.a = int(input("enter a: "))
+obj.b = int(input("enter b: "))
+
+print(obj.add())
