50be493b537b793048703b216c620c93270e66dc
Yokesh <yokeshbaskaran@gmail.com>
2026-05-07 17:56:00 +0530

    Polymorphism

---
+++ day-00/day3-4.py
@@ -0,0 +1,4 @@
+# Polymorphism
+# - it means: “Same method name, different behavior.”
+# - The word polymorphism means having many forms. In programming, polymorphism means the same function name (but different signatures) being used for different types.
+# - The key difference is the data types and number of arguments used in function.
