b2788c28e18e573933a63892d76e68936cd88e11
Yokesh <yokeshbaskaran@gmail.com>
2026-05-07 16:07:12 +0530

    Types of methods in Classes

--- day-00/day3-1.py
+++ day-00/day3-1.py
@@ -26,3 +26,44 @@ redmi.display()
 
 google = Phone("google", "18k")
 google.display()
+
+
+# Types of methods in Classes
+# 1. ➡️ Instance methods modify or access object-specific data. -> Works with object (instance) data. -> Uses self
+# 2. ➡️ Class methods modify or access class-wide data shared by all objects. -> Works with class-level data. Uses cls. -> Changes variables shared by all objects
+# 3. ➡️ Static methods are utility methods that don’t use object or class data. -> Independent of object and class data. -> No self or cls.
+
+
+class Laptop:
+    chargertype = "C TYPE"
+
+    def __init__(self):
+        self.brand = ""
+        self.price = 34
+
+    def setPrice(self, price):
+        # instance method
+        self.price = price
+
+    def getPrice(self):
+        print(self.price)
+
+    # class method
+    @classmethod
+    def setChargerType(cls):
+        cls.chargertype = "USB"
+        print("Changed type:")
+
+    # static method
+    @staticmethod
+    def info():
+        print("This is Lap class!")
+
+
+hp = Laptop()
+hp.getPrice()
+hp.setPrice(56000)
+hp.getPrice()
+Laptop.setChargerType()
+print(hp.chargertype)
+hp.info()
