266868aedb673d9fd1f20ee9cb167e54f2050291
Yokesh <yokeshbaskaran@gmail.com>
2026-05-07 12:12:59 +0530

    class example

--- day-00/day3-0.py
+++ day-00/day3-0.py
@@ -7,13 +7,36 @@
 # 🔹 What is an Object?
 # - An object is an instance of a class. It represents a real-world entity created from the class
 
+# 🔹 What is an constructor function?
+#  - A constructor is a unique function that gets called automatically when an object is created of a class.
+
 # 🔹 Key Concepts :
-# 3. Class = blueprint | Object = instance of class | Classes contain attributes and methods
-# 1. Attributes (Variables) -> Store data about the object || Example: brand, color
-# 2. Methods (Functions) -> Define behavior || Example: drive()
+# 1. Class = blueprint | Object = instance of class | Classes contain attributes and methods
+# 2. Attributes (Variables) -> Store data about the object || Example: brand, color
+# 3. Methods (Functions) -> Define behavior || Example: drive()
+# 4. self = refers to the current object and "self"  is NOT a keyword like if, for, while. But Python programmers always use self because it is the standard convention.
+# You can technically write:
+# def greet(me): and it still works.
+# person.greet() -> Python secretly changes it into: Greeting.greet(person) where Greeting -> class, .greet() -> method and person -> object
 
 
+# creating class
+class Greeting:
+    def greet():
+        print("Hi, Greetings!")
+
+
+Greeting.greet()
+
+
+# creating class without methods
+class Greeter:
+    pass
+
+
+# Classes and Objects - Examples
 class trip:
+    username = "enter name:"
     drink = ""
 
     def party(self):
@@ -22,8 +45,17 @@ class trip:
     def beach(self):
         print("Seeing sunsets")
 
+    def personDrinks(self):
+        print("slef:", self)
+
 
 ram = trip()
 sam = trip()
-ram.party()
-ram.beach()
+
+ram.username = "Ramu"
+ram.drink = "yes"
+print(ram.username, "drinks:", ram.drink)
+
+sam.username = "Sammy"
+sam.drink = "no"
+print(sam.username, "drinks:", sam.drink)
